6326555a2181d2da89a31af5ab660b59493574de
anil <9012kumaranil@gmail.com>
2025-09-06 12:29:05 +0530

    Crud Operation with Axios

---
+++ react_axios_crud/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Posts } from "./Components/Posts";
+
+function App() {
+  return (
+    <>
+      <section className="main-section">
+        <Posts />
+      </section>
+    </>
+  );
+}
+
+export default App;
